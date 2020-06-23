73aeb53c1d04a97870c7a91094bf4e12bc62b51a
Nasim <nasimsarwar07@gmail.com>
2020-06-23 20:04:29 +0530

    Hello

---
+++ build/precache-manifest.c52118d3dbd34179bb453adf2dff8b41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2bffb5f320f36ffe9d00a99e6dc4bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73636ac0490743bdafad",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d5036d724133fbab21f6",
+    "url": "/static/js/2.5d360bd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5d360bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73636ac0490743bdafad",
+    "url": "/static/js/main.6c20e1c4.chunk.js"
+  },
+  {
+    "revision": "166fda9bb7a5392399a3",
+    "url": "/static/js/runtime-main.d9254cfc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
